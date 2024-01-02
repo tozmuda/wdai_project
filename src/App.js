ebf18e90903754a3148110258d8aef6223a7c5df
Apiotr16st <146833883+Apiotr16st@users.noreply.github.com>
2024-01-02 12:24:48 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ProductList from './ProductList';
 import UpdateProductModal from './UpdateProductModal';
 import AddProductForm from './AddProductForm';
+import ShoppingCart from "./ShoppingCart";
 
 const App = () => {
   const [products, setProducts] = useState([]);
@@ -9,6 +10,16 @@ const App = () => {
   const [sortOrder, setSortOrder] = useState('default');
   const [searchInput, setSearchInput] = useState('');
   const [selectedProduct, setSelectedProduct] = useState(null);
+  const [cart, setCart] = useState([]);
+  const [cartTotal, setCartTotal] = useState(0);
+  const [expandedDescriptions, setExpandedDescriptions] = useState([]);
+  const [editingProduct, setEditingProduct] = useState(null);
+
+  useEffect(() => {
+    // Calculate the total sum whenever the cart changes
+    const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+    setCartTotal(total);
+  }, [cart]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,7 +52,7 @@ const App = () => {
 
   const searchProducts = () => {
     const filteredProducts = products.filter((product) =>
-      product.title.toLowerCase().includes(searchInput.toLowerCase())
+        product.title.toLowerCase().includes(searchInput.toLowerCase())
     );
 
     setFilteredProducts(filteredProducts);
@@ -49,7 +60,7 @@ const App = () => {
 
   const updateProduct = (productId, updatedData) => {
     const updatedProducts = products.map((product) =>
-      product.id === productId ? { ...product, ...updatedData } : product
+        product.id === productId ? { ...product, ...updatedData } : product
     );
 
     setProducts(updatedProducts);
@@ -57,63 +68,134 @@ const App = () => {
   };
 
   const deleteProduct = (productId) => {
-    
     const updatedProducts = products.filter((product) => product.id !== productId);
-
     setProducts(updatedProducts);
     setFilteredProducts(updatedProducts);
   };
 
   const addProduct = (newProduct) => {
-    
     const newProductId = Date.now();
-
     const addedProduct = { id: newProductId, ...newProduct };
-
     setProducts([...products, addedProduct]);
-    setFilteredProducts([...products, addedProduct]); 
+    setFilteredProducts([...products, addedProduct]);
+  };
+
+  const addToCart = (productId) => {
+    const selectedProduct = products.find((product) => product.id === productId);
+    if (selectedProduct) {
+      const existingCartItem = cart.find((item) => item.id === productId);
+      if (existingCartItem) {
+        setCart((prevCart) => prevCart.map((item) => item.id === productId ?
+            { ...item, quantity: item.quantity + 1 } : item));
+      } else {
+        setCart((prevCart) => [...prevCart, { ...selectedProduct, quantity: 1 }]);
+      }
+    }
+  };
+
+  const removeFromCart = (productId) => {
+    const selectedProduct = cart.find((item) => item.id === productId);
+    if (selectedProduct && selectedProduct.quantity > 1) {
+      setCart((prevCart) =>
+          prevCart.map((item) =>
+              item.id === productId ? { ...item, quantity: item.quantity - 1 } : item
+          )
+      );
+    } else {
+      setCart((prevCart) => prevCart.filter((item) => item.id !== productId));
+    }
+  };
+
+  const handleEditTitleChange = (e) => {
+    setEditingProduct((prev) => ({
+      ...prev,
+      title: e.target.value,
+    }));
+  };
+
+  const handleEditDescriptionChange = (e) => {
+    setEditingProduct((prev) => ({
+      ...prev,
+      description: e.target.value,
+    }));
+  };
+
+  const handleDoneEditing = () => {
+    const index = cart.findIndex((item) => item.id === editingProduct.id);
+    if (index !== -1) {
+      const updatedCart = [...cart];
+      updatedCart[index] = editingProduct;
+      setCart(updatedCart);
+    }
+    setEditingProduct(null);
+  };
+
+  const increaseCartQuantity = (productId) => {
+    const selectedCartItem = cart.find((item) => item.id === productId);
+
+    if (selectedCartItem) {
+      const updatedCart = cart.map((item) =>
+          item.id === productId ? { ...item, quantity: item.quantity + 1 } : item
+      );
+      setCart(updatedCart);
+    }
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        id="searchBar"
-        placeholder="Search by title"
-        value={searchInput}
-        onChange={(e) => setSearchInput(e.target.value)}
-      />
-      <button onClick={searchProducts}>Search</button>
-      <select id="myselect" onChange={(e) => sortData(e.target.value)}>
-        <option value="default">default</option>
-        <option value="asc">ascending</option>
-        <option value="desc">descending</option>
-      </select>
-      <AddProductForm onAddProduct={addProduct} />
-      <div id="productList">
-        <table>
-          <thead>
+      <div>
+        <input
+            type="text"
+            id="searchBar"
+            placeholder="Search by title"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button onClick={searchProducts}>Search</button>
+        <select id="myselect" onChange={(e) => sortData(e.target.value)}>
+          <option value="default">default</option>
+          <option value="asc">ascending</option>
+          <option value="desc">descending</option>
+        </select>
+        <AddProductForm onAddProduct={addProduct}/>
+        <div id="productList">
+          <table>
+            <thead>
             <tr>
               <th>Nazwa</th>
               <th>Opis</th>
               <th>Ikona</th>
+              <th>Action</th>
             </tr>
-          </thead>
-          <ProductList
-            products={filteredProducts}
-            onUpdateProduct={(productId, updatedData) => updateProduct(productId, updatedData)}
-            onDeleteProduct={(productId) => deleteProduct(productId)}
-          />
-        </table>
-      </div>
-      {selectedProduct && (
-        <UpdateProductModal
-          product={selectedProduct}
-          onUpdate={(productId, updatedData) => updateProduct(productId, updatedData)}
-          onClose={() => setSelectedProduct(null)}
+            </thead>
+            <ProductList
+                products={filteredProducts}
+                onUpdateProduct={(productId, updatedData) => updateProduct(productId, updatedData)}
+                onDeleteProduct={(productId) => deleteProduct(productId)}
+                onAddToCart={(productId) => addToCart(productId)}
+            />
+          </table>
+        </div>
+        <ShoppingCart
+            cart={cart}
+            cartTotal={cartTotal} // Pass cartTotal as a prop
+            expandedDescriptions={expandedDescriptions}
+            editingProduct={editingProduct}
+            increaseCartQuantity={increaseCartQuantity}
+            removeFromCart={removeFromCart}
+            handleEditTitleChange={handleEditTitleChange}
+            handleEditDescriptionChange={handleEditDescriptionChange}
+            handleDoneEditing={handleDoneEditing}
+            setExpandedDescriptions={setExpandedDescriptions}
+            setEditingProduct={setEditingProduct}
         />
-      )}
-    </div>
+        {selectedProduct && (
+            <UpdateProductModal
+                product={selectedProduct}
+                onUpdate={(productId, updatedData) => updateProduct(productId, updatedData)}
+                onClose={() => setSelectedProduct(null)}
+            />
+        )}
+      </div>
   );
 };
 
--- src/ProductList.js
+++ src/ProductList.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import UpdateProductModal from './UpdateProductModal';
 
-const ProductList = ({ products, onUpdateProduct, onDeleteProduct }) => {
+const ProductList = ({ products, onUpdateProduct, onDeleteProduct, onAddToCart }) => {
   const [expandedDescriptions, setExpandedDescriptions] = useState([]);
   const [selectedProduct, setSelectedProduct] = useState(null);
 
@@ -25,46 +25,53 @@ const ProductList = ({ products, onUpdateProduct, onDeleteProduct }) => {
   };
 
   const deleteProduct = (productId) => {
-    // Trigger the onDeleteProduct callback to delete the product
     if (onDeleteProduct) {
       onDeleteProduct(productId);
     }
   };
 
+  const addToCart = (productId) => {
+    if (onAddToCart) {
+      onAddToCart(productId);
+    }
+  };
+
   return (
-    <tbody>
+      <tbody>
       {products.map((product) => (
-        <tr key={product.id}>
-          <td>{product.title}</td>
-          <td>
-            {expandedDescriptions.includes(product.id)
-              ? product.description
-              : `${product.description.slice(0, 50)}...`}
-            <button onClick={() => toggleDescription(product.id)}>
-              {expandedDescriptions.includes(product.id) ? 'Read Less' : 'Read More'}
-            </button>
-          </td>
-          <td>
-            <img src={product.thumbnail} alt="Product Icon" style={{ maxWidth: '50px', maxHeight: '50px' }} />
-          </td>
-          <td>
-            <button onClick={() => openUpdateModal(product)}>Update</button>
-          </td>
-          <td>
-            <button onClick={() => deleteProduct(product.id)}>Delete</button>
-          </td>
-        </tr>
+          <tr key={product.id}>
+            <td>{product.title}</td>
+            <td>
+              {expandedDescriptions.includes(product.id)
+                  ? product.description
+                  : `${product.description.slice(0, 50)}...`}
+              <button onClick={() => toggleDescription(product.id)}>
+                {expandedDescriptions.includes(product.id) ? 'Read Less' : 'Read More'}
+              </button>
+            </td>
+            <td>
+              <img src={product.thumbnail} alt="Product Icon" style={{ maxWidth: '50px', maxHeight: '50px' }} />
+            </td>
+            <td>
+              <button onClick={() => openUpdateModal(product)}>Update</button>
+            </td>
+            <td>
+              <button onClick={() => deleteProduct(product.id)}>Delete</button>
+            </td>
+            <td>
+              <button onClick={() => addToCart(product.id)}>Add to Cart</button>
+            </td>
+          </tr>
       ))}
       {selectedProduct && (
-        <UpdateProductModal
-          product={selectedProduct}
-          onUpdate={onUpdateProduct}
-          onClose={closeUpdateModal}
-        />
+          <UpdateProductModal
+              product={selectedProduct}
+              onUpdate={onUpdateProduct}
+              onClose={closeUpdateModal}
+          />
       )}
-    </tbody>
+      </tbody>
   );
 };
 
 export default ProductList;
-
